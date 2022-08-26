bbc6520b4298777e7d608ddb080069395514593a
kornelia <korazielinska@gmail.com>
2022-08-26 16:08:13 +0200

    storzenie folderu

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function odwrocString(string) {
+    let result = string.split('');
+    console.log(result)
+    return result;
+}
+
+console.log(odwrocString("Akademia108"));
+
+let string = "Akademia 108";
+console.log(string);
+let output = string.split("").reverse().join("");
+
+console.log(output)
